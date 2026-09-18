162a64dff2d13b580fcbe31bc514f49c5cb71c5b
Ethan Nijmeh <ethannij2@gmail.com>
2026-09-18 01:30:04 -0700

    didnt save autostart changes

--- config/hypr/hyprland/autostart.lua
+++ config/hypr/hyprland/autostart.lua
@@ -3,10 +3,4 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("qs") -- start quickshell
 	hl.exec_cmd("hypridle") -- idle daemon
 	hl.exec_cmd("hyprsunset") -- Required
-	hl.exec_cmd(
-		'inotifywait -m -q -e close_write,moved_to,create --format "%f" '
-			.. '"$HOME/.local/state/quickshell" | while read -r f; do '
-			.. '[ "$f" = "states.json" ] && hyprctl eval "apply_gamemode_from_state()"; '
-			.. "done"
-	) -- watch quickshell states and check/apply gamemode on changes
 end)
